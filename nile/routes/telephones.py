943cf057e29122c8ed96f2f6c9a67043f40e70d4
MichalMoravik <michal.moravik@icloud.com>
2020-06-03 14:39:57 +0200

    telephone routes done. SQL routes and model finished

--- nile/routes/telephones.py
+++ nile/routes/telephones.py
@@ -27,72 +27,63 @@ def add_telephone():
         print(f"Error: {e}")
         return jsonify("Cannot add telephone!"), 500
 
-# # Get all telephones
-# @app.route('/telephone', methods=['GET'])
-# def get_telephones():
-#     try:
-#         all_telephones = telephone.query.all()
-#         result = telephones_schema.dump(all_telephones)
-#         return jsonify(result)
-#     except Exception as e:
-#         print(f"******* Error in routes/telephones.py: get_telephones() *******")
-#         print(f"Error: {e}")
-#         return jsonify("Cannot get telephones!"), 500
 
-# # Get one telephone
-# @app.route('/telephone/<id>', methods=['GET'])
-# def get_telephone(id):
-#     try:
-#         telephone = telephone.query.get(id)
-#         return telephone_schema.jsonify(telephone)
-#     except Exception as e:
-#         print(f"******* Error in routes/telephones.py: get_telephone(id) *******")
-#         print(f"Error: {e}")
-#         return jsonify("Cannot get telephone!"), 500
+# Get all telephone numbers
+@app.route('/telephone', methods=['GET'])
+def get_telephones():
+    try:
+        all_telephones = Telephone.query.all()
+        result = telephones_schema.dump(all_telephones)
+        return jsonify(result)
+    except Exception as e:
+        print(f"******* Error in routes/telephones.py: get_telephones() *******")
+        print(f"Error: {e}")
+        return jsonify("Cannot get telephones!"), 500
 
-# # Update a telephone
-# @app.route('/telephone/<id>', methods=['PUT'])
-# def update_telephone(id):
-#     try:
-#         delivery_address = request.json['delivery_address']
-#         status = request.json['status']
-#         fk_customer_id = request.json['fk_customer_id']
-#         productsIdArray = request.json['productsIdArray']
-        
-#         # find customer in the database by id
-#         customer = Customer.query.filter_by(id=fk_customer_id).first()
+# Get one telephone
+@app.route('/telephone/<id>', methods=['GET'])
+def get_telephone(id):
+    try:
+        telephone = Telephone.query.get(id)
+        return telephone_schema.jsonify(telephone)
+    except Exception as e:
+        print(f"******* Error in routes/telephones.py: get_telephone(id) *******")
+        print(f"Error: {e}")
+        return jsonify("Cannot get telephone!"), 500
 
-#         # find products in the database by id
-#         products = []
-#         for product_id in productsIdArray:
-#             product = Product.query.filter_by(id=product_id).first()
-#             products.append(product)
+# Update a telephone
+@app.route('/telephone/<id>', methods=['PUT'])
+def update_telephone(id):
+    try:
+        fk_customer_id = request.json['fk_customer_id']
+        telephone_number = request.json['telephone_number']
+        # find customer in the database by id
+        # object is needed in order to save via sqlalchemy
+        customer = Customer.query.filter_by(id=fk_customer_id).first()
 
-#         telephone = telephone.query.get(id)
-#         telephone.delivery_address = delivery_address
-#         telephone.status = status
-#         telephone.fk_customer_id = fk_customer_id
-#         telephone.customer = customer
-#         telephone.products = products
+        telephone = Telephone.query.get(id)
+        telephone.telephone_number = telephone_number
+        telephone.fk_customer_id = fk_customer_id
+        telephone.customer = customer
 
-#         db.session.commit()
+        db.session.commit()
 
-#         return telephone_schema.jsonify(telephone)
-#     except Exception as e:
-#         print(f"******* Error in routes/telephones.py: update_telephone(id) *******")
-#         print(f"Error: {e}")
-#         return jsonify("Cannot update product!"), 500
+        return telephone_schema.jsonify(telephone)
+    except Exception as e:
+        print(f"******* Error in routes/telephones.py: update_telephone(id) *******")
+        print(f"Error: {e}")
+        return jsonify("Cannot update product!"), 500
 
-# # Delete telephone
-# @app.route('/telephone/<id>', methods=['DELETE'])
-# def delete_telephone(id):
-#     try:
-#         telephone = telephone.query.get(id)
-#         db.session.delete(telephone)
-#         db.session.commit()
+# Delete telephone
+@app.route('/telephone/<id>', methods=['DELETE'])
+def delete_telephone(id):
+    try:
+        telephone = Telephone.query.get(id)
+        db.session.delete(telephone)
+        db.session.commit()
 
-#         return telephone_schema.jsonify(telephone)
-#     except Exception as e:
-#         print(f"******* Error in routes/telephones.py: delete_telephone(id) *******")
-#         print(f"Error: {e}")
-#         return jsonify("Cannot delete telephone!"), 500
+        return telephone_schema.jsonify(telephone)
+    except Exception as e:
+        print(f"******* Error in routes/telephones.py: delete_telephone(id) *******")
+        print(f"Error: {e}")
+        return jsonify("Cannot delete telephone!"), 500
